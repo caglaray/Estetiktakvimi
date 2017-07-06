b12426ddf2ef894057c8b1f69a97ef707f76df78
caglaray <cancaglaray@gmail.com>
2017-07-06 10:34:24 +0300

    duyuru

--- site/resources/views/docadmin/duyuru.blade.php
+++ site/resources/views/docadmin/duyuru.blade.php
@@ -6,43 +6,21 @@
 <div class="row">
   <div class="widget">
   					<div class="widget-header transparent">
-  						<h2><strong>Form</strong> Element</h2>
-  						<div class="additional-btn">
-  							<a href="#" class="hidden reload"><i class="icon-ccw-1"></i></a>
-  							<a href="#" class="widget-toggle"><i class="icon-down-open-2"></i></a>
-  							<a href="#" class="widget-close"><i class="icon-cancel-3"></i></a>
-  						</div>
+  						<h2><strong>Duyuru</strong> Yolla</h2>
+
   					</div>
   					<div class="widget-content padding">
   						<form class="form-horizontal" role="form">
-  						
+
   						  <div class="form-group">
-  							<label class="col-sm-2 control-label">Static Control</label>
+  							<label class="col-sm-2 control-label">Gönderen :</label>
   							<div class="col-sm-10">
-  							  <p class="form-control-static">email@example.com</p>
-  							</div>
-  						  </div>
-  						  <div class="form-group">
-  							<label class="col-sm-2 control-label">Radio</label>
-  							<div class="col-sm-10">
-  								<div class="radio iradio">
-  								  <label>
-  									<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-  									Option one is this and that&mdash;be sure to include why it's great
-  								  </label>
-  								</div>
-  								<div class="radio iradio">
-  								  <label>
-  									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-  									Option two can be something else and selecting it will deselect option one
-  								  </label>
-  								</div>
+  							  <p class="form-control-static">{{Auth::user()->email}}</p>
   							</div>
   						  </div>
 
-
   						  <div class="form-group">
-  							<label class="col-sm-2 control-label">WYSWYG</label>
+  							<label class="col-sm-2 control-label">Duyuru Metni :</label>
   							<div class="col-sm-10">
   							  <textarea class="summernote"></textarea>
   							</div>
